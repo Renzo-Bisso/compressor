cb87262febd2636982a389b8aba1b10fd710a97b
Renzo Bisso <renzo.bissoo@gmail.com>
2022-09-25 22:00:58 -0300

    Nueva funcionalidad para organizar
    Agregamos una funcion que nos permite poder crear una carpeta en el llegado caso que no este y guardemos ahi las imagenes comprimidas, si la carpeta ya esta, las guarda directamente

--- script.py
+++ script.py
@@ -1,17 +1,28 @@
 # Llamo a la libreria Pillow que nos permite poder comprimir las imagenes
 from PIL import Image
+from os import mkdir
+import shutil
 import os  # Iportamos OS que nos permite poder leer los archivos de la carpetas
 
-# definimos la carpeta a donde vamos asir a bucar los archivos descargados
+# verificamos si existe la carpeta, si no existe la creamos
+aux = os.path.isdir(r"../../../Downloads/comprimido/")
+
+if aux == False:
+    mkdir("../../../Downloads/comprimido/")
+
+# definimos la ruta de destino
+file_destination = ("../../../Downloads/comprimido/")
+# definimos la carpeta a donde vamos a buscar los archivos descargados
 downloadsFolder = "../../../Downloads/"
 
+
 if __name__ == "__main__":  # preguntamos si el archivo que estamos ejecutando desde la terminal
-    # iteramos todos loarchivos que se encuentran dentro de esta carpeta y le damos el nombre de filename
+    # iteramos todos los rchivos que se encuentran dentro de esta carpeta y le damos el nombre de filename
     for filename in os.listdir(downloadsFolder):
-        # tomamos el nombre del archivo y luego lo separamos entre el nombr edel archivo y su exension
+        # tomamos el nombre del archivo y luego lo separamos entre el nombre del archivo y su exension
         name, extension = os.path.splitext(downloadsFolder + filename)
         if extension in [".jpg", ".jpeg", ".png"]:  # preguntamos su extencion
             # abrimos esta imagen, la intruccion de open permite manipularlo mejor  el codigo
             picture = Image.open(downloadsFolder + filename)
-            picture.save(downloadsFolder + "comprimido_" +
-                         filename, optimize=True, quality=60)  # lo guardamos en la misma carpeta pero agregamos la palabra comprimido al archivo
+            picture.save(file_destination + "comprimido_" +
+                         filename, optimize=True, quality=60)  # lo guardamos en la carpeta que creamos anteriormente pero agregamos la palabra comprimido al archivo
